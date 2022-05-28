99c5e4adc88cd3af3f312cc40991e8fb72eef2d7
ABHISHEK-RAJ <abhishek0069.be21@chitkara.edu.in>
2022-05-29 02:13:23 +0530

    abhishek

---
+++ pattern.cpp
@@ -0,0 +1,16 @@
+include <iostream>
+using namespace std;
+ 
+// Function to demonstrate printing pattern
+void pypart(int n)
+{
+}
+           
+ 
+// Driver Function
+int main()
+{
+    int n = 5;
+    pypart(n);
+    return 0;
+}
